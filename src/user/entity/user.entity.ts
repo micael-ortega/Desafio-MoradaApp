16e98758fc9ef415953014bac82ca6fcccf2d419
micael-ortega <micaelortega1@gmail.com>
2024-03-24 10:02:17 -0300

    feat: conecta api ao postgres

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,9 +1,45 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity('users')
 export class User {
+    @PrimaryGeneratedColumn('uuid')
     id: string
+
+    @Column({
+        nullable: false,
+        type: 'varchar',
+    })
     name: string
+
+    @Column({
+        nullable: false,
+        type: 'varchar',
+        unique: true,
+    })
     email: string
+
+    @Column({
+        nullable: false,
+        type: 'varchar',
+    })
     description: string
+
+    @Column({
+        nullable: false,
+        type: 'char',
+    })
     password: string
+
+    @Column({
+        type: "timestamp without time zone",
+        default: () => 'CURRENT_TIMESTAMP'
+    })
     createdAt: Date
+
+
+    @Column({
+        type: "timestamp without time zone",
+        default: () => 'CURRENT_TIMESTAMP'
+    })
     updateAt: Date
 }
